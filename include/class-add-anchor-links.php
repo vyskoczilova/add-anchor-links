7b3a92024bebcf4cb767e49a16401509592563ba
pewgeuges <73141620+pewgeuges@users.noreply.github.com>
2020-10-24 05:40:16 +0200

    Update class-add-anchor-links.php
    recovered improved comments

--- include/class-add-anchor-links.php
+++ include/class-add-anchor-links.php
@@ -7,7 +7,7 @@
  * Forked on GitHub by pewgeuges on 2020-10-20T0159+0200
  * This code has been customized on 2020-10-18T2246+0200
  * to handle paragraphs instead of headings.
- * Last modified:  2020-10-24T0143+0200
+ * Last modified:  2020-10-24T0538+0200
  */
 // If this file is called directly, abort:
 if ( ! defined( 'WPINC' ) ) {
@@ -60,14 +60,15 @@ if ( ! class_exists( 'Add_Anchor_Links' ) ) {
 
                         // converting accented characters to ASCII meets user expectations,
                         // but the function remove_accents() called by sanitize_title(),
-                        // mis-handling the German 'ß', is a bad function as-is. Fix:
+                        // mis-handles the German 'ß' and is therefore not recommended.
+                        // Fix:
                         $paragraph = str_replace( 'ß', 'ss', $paragraph );  // useful default, not German-only
                         $paragraph = str_replace( 'ẞ', 'SS', $paragraph );  // now we have uppercase too
                         $paragraph = str_replace( '£', 'L', $paragraph );   // euro is E, why not pound L
-                        $anchor = remove_accents( $paragraph ); // bad function as-is, called by sanitize_title()
-                        //$anchor = sanitize_title( $paragraph );
+                        $anchor = remove_accents( $paragraph ); // sub-optimal function as-is, called by:
+                        //$anchor = sanitize_title( $headline );
                         $anchor = sanitize_title_with_dashes( $anchor, $anchor, 'save' );  // performs better.
-                        // $context must be 'save', or a bunch of characters won’t be stripped off.
+                        //                        $context must be 'save', or some characters won’t be deleted.
                         $anchor = str_replace( '%e2%80%af', '', $anchor );  // NNBSP missing from the list.
 
                         $icon = '<a href="#' . $anchor . '" aria-hidden="true" class="aal_anchor" id="' . $anchor . '"><svg aria-hidden="true" class="aal_svg" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>';
